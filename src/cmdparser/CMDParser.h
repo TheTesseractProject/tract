b153228f563c6aee33b00c768fadb156ed450a24
pka-human <o0o0.pe4Ka.0o0o@gmail.com>
2025-01-25 02:45:30 +0500

    change structure

--- src/cmdparser/CMDParser.h
+++ src/cmdparser/CMDParser.h
@@ -1,7 +1,15 @@
 #ifndef CMDPARSER_H
 #define CMDPARSER_H
 
-#include "typedefs.h"
+#include <stdbool.h>
+
+typedef struct {
+    bool error;
+    bool version_flag;
+    bool help_flag;
+    bool build_flag;
+    unsigned path_argument;
+} CMDdata;
 
 CMDdata parsecmd(int *argc, char* argv[]);
 
